5973e2c133ced19783ee97f415cfaf203114a315
jungefuchs <rony.se7@gmail.com>
2022-11-17 00:10:15 +0700

    Update Player.cs
    rotate back to default

--- Assets/_Scripts/Core/Player.cs
+++ Assets/_Scripts/Core/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 namespace RS.Typing.Core {
     public class Player : MonoBehaviour {
         [SerializeField] private float angleOffset = 90f;
+
+        private float _timeRefresh = float.MaxValue;
         private void Awake() {
             WordObject.WordMatched += WordObjectOnWordMatched;
         }
@@ -20,14 +22,20 @@ namespace RS.Typing.Core {
             var direction = (wordObject.transform.position - transform.position).normalized;
             var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            transform.DORotateQuaternion(Quaternion.Euler(0, 0, angle-angleOffset), .15f);
+            transform.DORotateQuaternion(Quaternion.Euler(0, 0, angle-angleOffset), .15f).OnComplete(() => {
+                _timeRefresh = Time.time + .25f;
+            });
         }
 
-        private void Start() {
-        
-        }
         private void Update() {
-        
+            if (Time.time > _timeRefresh) {
+                Refresh();
+            }
+        }
+
+        private void Refresh() {
+            transform.DORotateQuaternion(Quaternion.Euler(0, 0, 0), .15f);
+            _timeRefresh = float.MaxValue;
         }
     }
 }
