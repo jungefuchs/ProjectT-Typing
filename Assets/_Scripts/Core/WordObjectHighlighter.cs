fcefa2f827a592dab210f2f2e5796cf60c36743e
jungefuchs <rony.se7@gmail.com>
2022-11-17 08:31:51 +0700

    adding wordObjectHiglighter to highlight word when typed correctly

---
+++ Assets/_Scripts/Core/WordObjectHighlighter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RS.Typing.Core {
+    public class WordObjectHighlighter : MonoBehaviour {
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private Material highlightedTextMaterial;
+        [SerializeField] private Material defaultTextMaterial;
+        [SerializeField] private Image backgroundTextImage;
+
+        private WordObject _wordObject;
+
+        private void Awake() {
+            _wordObject = GetComponent<WordObject>();
+            WordObject.WordMatched += WordObjectOnWordMatched;
+        }
+
+        private void OnDestroy() {
+            WordObject.WordMatched -= WordObjectOnWordMatched;
+        }
+
+        private void WordObjectOnWordMatched(object sender, bool isMatch) {
+            if (sender is WordObject wordObject && wordObject == _wordObject) {
+                text.fontMaterial = isMatch ? highlightedTextMaterial : defaultTextMaterial;
+                backgroundTextImage.enabled = isMatch;
+            }
+            
+            
+        }
+
+        private void OnDisable() {
+            text.fontMaterial = defaultTextMaterial;
+            backgroundTextImage.enabled = false;
+        }
+    }
+}
